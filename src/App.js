5c601d8e2436e906f970d9c2c5bd74a8b6d21b4f
Yevgeniy <yevgeniyerot@gmail.com>
2019-05-15 18:14:11 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import LoginForm from "./components/LoginForm";
-import TwitterMessage from "./components/TwitterMessage";
+import React, {Component} from 'react'
+import LoginForm          from "./components/LoginForm";
+import TwitterMessage     from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
+  login = ({username, password}) => {
     console.log(`Logging in ${username} with password ${password}`);
   };
 
@@ -15,13 +15,12 @@ class App extends Component {
         <h1>
           <pre>LoginForm</pre>
         </h1>
-        <LoginForm handleLogin={this.login} />
+        <LoginForm handleLogin={this.login}/>
 
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
-        <TwitterMessage maxChars={140} />
-
+        <TwitterMessage maxChars={140}/>
 
 
       </div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  state = {
+    value: ''
+  };
 
-    this.state = {};
-  }
+  handleChange = e => {
+    const max = this.props.maxChars;
+    if ( this.state.value.length < max ) {
+      this.setState({
+        value: e.target.value
+      })
+    }
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" value={this.state.value} name="message" id="message" onChange={this.handleChange}/>
+        Characters left: {this.props.maxChars - this.state.value.length}
       </div>
     );
   }
